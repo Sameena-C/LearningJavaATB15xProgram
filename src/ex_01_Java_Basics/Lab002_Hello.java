6c71ab4847cfaa13c21fb757d62ab8e20813266b
Sameena-C <sameena.c@gmail.com>
2025-12-17 13:01:10 +0530

    Hello

---
+++ src/ex_01_Java_Basics/Lab002_Hello.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("This is Java ATB15x course");
+    }
+}
